7bcc424cc6f4836d1887410f305f63ffd4f1cd32
Vallem Balu <vbnreddy5@gmail.com>
2021-07-15 15:59:17 +0530

    Vending machine money count

---
+++ VendingMachine/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter the amount : ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            int[] notes = new int[] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int[] Notescounter = new int[7];
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    Notescounter[i] = amount / notes[i];
+                    amount = amount - Notescounter[i] * notes[i];
+                }
+            }
+
+            Console.WriteLine("currency count by vending machine ");
+            int NoOfNotes = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (Notescounter[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : " + Notescounter[i]);
+                    NoOfNotes += Notescounter[i];
+                }
+            }
+            Console.WriteLine("Minimum number of notes : " + NoOfNotes);
+        }
+    }
+}

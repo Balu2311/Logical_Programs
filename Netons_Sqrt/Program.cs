b6d2ecc1941c4a5c8126af94eb7153e7d1282da4
Vallem Balu <vbnreddy5@gmail.com>
2021-07-15 16:12:05 +0530

    Newtons Sqrt

---
+++ Netons_Sqrt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Newtons_Sqrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Newton's method:");
+            Console.WriteLine("Enter the value of c :");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double epsilon = 1e-15;
+            double t = c;
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (c / t + t) / 2.0;
+            }
+            Console.WriteLine(t);
+        }
+    }
+}
